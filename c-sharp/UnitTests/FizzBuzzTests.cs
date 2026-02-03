dbe36a9f60acdef39b06909e5c8d2daa5457aa5b
Kris Bunda <14361990+bunda3d@users.noreply.github.com>
2026-02-03 07:18:21 +0000

    FizzBuzz refactor tests

--- c-sharp/UnitTests/FizzBuzzTests.cs
+++ c-sharp/UnitTests/FizzBuzzTests.cs
@@ -1,4 +1,3 @@
-
 using Application;
 
 namespace UnitTests;
@@ -13,55 +12,18 @@ namespace UnitTests;
     2. If the number is divisible by 5, return "Buzz".
     3. If the number is divisible by both 3 and 5, return "FizzBuzz".
     4. Otherwise, return the number as a string (e.g., 1 -> "1").
-
-    Step-by-Step TDD Guide:
-    - Test 1: Input 1 -> Returns "1"
-    - Test 2: Input 2 -> Returns "2"
-    - Test 3: Input 3 -> Returns "Fizz"
-    - Test 4: Input 5 -> Returns "Buzz"
-    - Test 5: Input 6 -> Returns "Fizz" (Test multiple of 3)
-    - Test 6: Input 10 -> Returns "Buzz" (Test multiple of 5)
-    - Test 7: Input 15 -> Returns "FizzBuzz"
-    - Test 8: Input 30 -> Returns "FizzBuzz" (Test multiple of 3 & 5)
 */
 #endregion
 
 public class FizzBuzzTests
 {
   [Theory]
-  [InlineData(1, "1")]
+  [InlineData(1, "1")]  // Default case: not divisible evenly
   [InlineData(2, "2")]
   [InlineData(3, "Fizz")]
-  public void GetStatus_ReturnsFizz_IfDivisibleBy3(int input, string returnStatus)
-  {
-    // Given
-    var fizzBuzz = new FizzBuzz();
-
-    // When
-    var result = fizzBuzz.GetStatus(input);
-
-    // Then
-    Assert.Equal(returnStatus, result);
-  }
-
-  [Theory]
   [InlineData(5, "Buzz")]
-  public void GetStatus_ReturnsBuzz_IfDivisibleBy5(int input, string returnStatus)
-  {
-    // Given
-    var fizzBuzz = new FizzBuzz();
-
-    // When
-    var result = fizzBuzz.GetStatus(input);
-
-    // Then
-    Assert.Equal(returnStatus, result);
-  }
-
-
-  [Theory]
   [InlineData(15, "FizzBuzz")]
-  public void GetStatus_ReturnsFizzBuzz_IfDivisibleBy3And5(int input, string returnStatus)
+  public void GetStatus_ReturnsCorrectStatus_PerDivisibility(int input, string returnStatus)
   {
     // Given
     var fizzBuzz = new FizzBuzz();
